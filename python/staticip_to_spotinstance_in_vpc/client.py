bcad51fd94ebe8384e72a25a0361d5151392bc78
yuta imai <imai.factory@gmail.com>
2012-10-07 16:26:53 +0000

    modify option

--- python/staticip_to_spotinstance_in_vpc/client.py
+++ python/staticip_to_spotinstance_in_vpc/client.py
@@ -9,10 +9,13 @@ import boto.ec2
 parser = optparse.OptionParser()
 parser.add_option('-e', '--eni_id', dest='eni_id')
 parser.add_option('-r', '--iam_role', dest='iam_role')
+parser.add_option('-t', '--target_iam_role', dest='target_iam_role')
 options, reminder = parser.parse_args()
 
 # create ec2connection instance
-response = urllib2.urlopen('http://169.254.169.254/latest/meta-data/iam/security-credentials/' + options.iam_role)
+response =
+urllib2.urlopen('http://169.254.169.254/latest/meta-data/iam/security-credentials/'
++ options.target_iam_role)
 credential = json.loads(response.read())
 ec2connection = boto.ec2.connect_to_region(
 	'ap-northeast-1',
